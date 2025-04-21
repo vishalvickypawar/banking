cc89ac866b32b1bdeb45833ed208ae835bcae63f
Vishal Pawar <168279073+vishalvickypawar@users.noreply.github.com>
2025-04-21 21:57:00 +0530

    Revert "Added user authentication and authorization using basic auth with spring security"
    This reverts commit 4e0430cab2839f477ecaa9030ad25c1b2166849b.

--- src/main/java/com/example/banking/security/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.banking.security;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByUsername(String username);
-}
